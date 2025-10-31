a166c3130795536d7574f02930e08f0a7f9e2ba6
N1NTENDO1999 <denysnazarko99@gmail.com>
2025-10-31 14:36:08 +0100

    Added functionality to send astm messages and fixed async test execution

--- Services/Protocols/AstmService.cs
+++ Services/Protocols/AstmService.cs
@@ -1,14 +1,19 @@
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
+using System.Text;
 
 namespace Services.Protocols
 {
     public class AstmService : BaseProtocol, IProtocolService
     {
-        private const byte STX = 0x02;
-        private const byte ETX = 0x03;
-        private const byte CR = 0x0D;
+        private const byte ENQ = 0x05;
+        private const byte EOT = 0x04;
+        private const char CR = (char)0x0D;
+        private const char STX = (char)0x02;
+        private const char ETX = (char)0x03;
+        private const char LF = (char)0x0A;
+        private const byte ACK = 0x06;
 
-        public AstmService( IConfiguration configuration) : base(configuration)
+        public AstmService(IConfiguration configuration) : base(configuration)
         {
         }
 
@@ -22,21 +27,35 @@ namespace Services.Protocols
             try
             {
                 var stream = _stream ?? client.GetStream();
-                var payload = new System.Text.UTF8Encoding().GetBytes(message + (char)CR);
-                var framed = new byte[1 + payload.Length + 1];
-                framed[0] = STX;
-                Buffer.BlockCopy(payload, 0, framed, 1, payload.Length);
-                framed[1 + payload.Length] = ETX;
-                await stream.WriteAsync(framed, 0, framed.Length, cancellationToken);
-                await stream.FlushAsync();
+                //Covers the edgecase when message has \r\n. If not message will be send as one chunk
+                var messageShards = message.Split("\r\n");
 
-                byte[] buffer = new byte[4096];
-                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-                if (bytesRead > 0)
+                await stream.WriteAsync(new[] { ENQ });
+
+                var ack = stream.ReadByte();
+
+                if (ack != ACK)
                 {
-                    // What should we do with the ACK?
-                    string ack = new System.Text.UTF8Encoding().GetString(buffer, 0, bytesRead);
+                    //If status is not ACK that means the host is busy, what should we do in this case?
                 }
+
+                for (var i = 0; i < messageShards.Length; i++)
+                {
+                    var framedMessage = BuildFramedMessage(messageShards[i], i + 1);
+
+                    var messageBytes = Encoding.ASCII.GetBytes(framedMessage);
+
+                    await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+                    var frameAck = stream.ReadByte();
+
+                    if (frameAck != ACK)
+                    {
+                        // Same here
+                    }
+                }
+
+                await stream.WriteAsync(new[] { EOT });
 
                 client.Dispose();
                 stream.Dispose();
@@ -45,6 +64,27 @@ namespace Services.Protocols
             {
                 throw new InvalidOperationException($"Error sending ASTM message: {ex.Message}");
             }
+        }
+
+        private string CalculateHexSumOfPlainMessage(string message, int index)
+        {
+            int sum = 0;
+            var messageFrame = index.ToString() + message + CR + ETX;
+            foreach (char c in messageFrame)
+            {
+                sum += c; 
+                if (c == ETX)
+                    break;
+            }
+
+            var hexSum = sum.ToString("X");
+            return hexSum.Length >= 2 ? hexSum[^2..] : hexSum.PadLeft(2, '0');
+        }
+
+        private string BuildFramedMessage(string message, int index)
+        {
+            var hexSum = CalculateHexSumOfPlainMessage(message, index);
+            return STX + index.ToString() + message + CR + ETX + hexSum + CR + LF;
         }
     }
 }
